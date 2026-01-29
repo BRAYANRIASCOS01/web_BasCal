49c2d54844f1ad46d1bf4080525882877798e7ca
joburbanop <joburbanop589@gmail.com>
2026-01-29 13:45:04 -0500

    eliminacion de elemtos huerfanos

--- src/shared/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Navbar } from "./Navbar";
-export { default as Contact } from "./Contact";
-export { default as ScrollTop } from "./ScrollTop";
-export { default as WhatsAppButton } from "./WhatsAppButton";
-export { default as Hero } from "./Hero";
